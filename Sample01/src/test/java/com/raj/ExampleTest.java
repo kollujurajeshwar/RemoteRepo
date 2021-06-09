fb392ebe3c83ece199c16d6c435eda1a72abbe26
kollujurajeshwar <kollujurajeshwar@gmail.com>
2021-06-09 15:45:07 +0530

    Initial Commit

---
+++ Sample01/src/test/java/com/raj/ExampleTest.java
@@ -0,0 +1,17 @@
+package com.raj;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test1() {
+		Assert.fail();
+	}
+
+	@Test
+	public void test2() {
+		Assert.fail();
+	}
+}
